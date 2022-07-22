6e2ebc00701313c9416cf342e1c91a7bb97ec932
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-07-22 18:18:42 -0300

    Atualiza o arquivo bin/snip.neovim.complete.sh

--- bin/snip.neovim.complete.sh
+++ bin/snip.neovim.complete.sh
@@ -16,16 +16,16 @@ if test -e $_f ; then
     echo UltiSnipets > $_f
     ln -s ${_t}.UltiSnipets ${_t}
 
+    rm -f ${_s}/cmp_luasnip
+    rm -f ${_s}/LuaSnip
+
+    ln -s ${_a}/nvim-notify        ${_s}/nvim-notify
+    ln -s ${_a}/Comment.nvim       ${_s}/Comment.nvim
     ln -s ${_a}/noah.vim           ${_s}/noah.vim
     ln -s ${_a}/ultisnips          ${_s}/ultisnips
     ln -s ${_a}/vim-snippets       ${_s}/vim-snippets
     ln -s ${_a}/vim-pythonx        ${_s}/vim-pythonx
     ln -s ${_a}/cmp-nvim-ultisnips ${_s}/cmp-nvim-ultisnips
-    ln -s ${_a}/nvim-notify        ${_s}/nvim-notify
-    ln -s ${_a}/Comment.nvim       ${_s}/Comment.nvim
-
-    rm -f ${_s}/cmp_luasnip
-    rm -f ${_s}/LuaSnip
 
     rm $_f
 else
@@ -33,16 +33,18 @@ else
     echo LuaSnippets > $_f
     ln -s ${_t}.LuaSnippets ${_t}
 
-    ln -s ${_a}/cmp_luasnip ${_s}/cmp_luasnip
-    ln -s ${_a}/LuaSnip     ${_s}/LuaSnip
-
-    rm -f ${_s}/noah.vim
-    rm -f ${_s}/nvim-notify
     rm -f ${_s}/Comment.nvim
+    rm -f ${_s}/nvim-notify
+    rm -f ${_s}/noah.vim
     rm -f ${_s}/ultisnips
     rm -f ${_s}/vim-snippets
     rm -f ${_s}/vim-pythonx
     rm -f ${_s}/cmp-nvim-ultisnips
+
+    ln -s ${_a}/cmp_luasnip        ${_s}/cmp_luasnip
+    ln -s ${_a}/LuaSnip            ${_s}/LuaSnip
+    ln -s ${_a}/nvim-notify        ${_s}/nvim-notify
+    ln -s ${_a}/Comment.nvim       ${_s}/Comment.nvim
 fi
 
 exit 0
