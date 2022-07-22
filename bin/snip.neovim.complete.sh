1d996eb003f53a4f2c221bfe3cfda3f579d2143b
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-07-22 18:55:45 -0300

    Atualiza o arquivo bin/snip.neovim.complete.sh

--- bin/snip.neovim.complete.sh
+++ bin/snip.neovim.complete.sh
@@ -5,7 +5,7 @@
 
 # if exist file `_f' then remove.
 _t=~/.config/nvim/plugin/packer_compiled.lua
-test -L $_t && rm $_t
+test -L $_t && rm -rf $_t
 
 _f=/tmp/snippets
 _s=${HOME}/.local/share/nvim/site/pack/packer/start
@@ -18,10 +18,10 @@ if test -e $_f ; then
     echo UltiSnipets > $_f
     ln -s ${_t}.UltiSnipets ${_t}
 
-    rm ${_s}/Comment.nvim
-    rm ${_s}/nvim-notify
-    rm ${_s}/cmp_luasnip
-    rm ${_s}/LuaSnip
+    rm -rf ${_s}/Comment.nvim
+    rm -rf ${_s}/nvim-notify
+    rm -rf ${_s}/cmp_luasnip
+    rm -rf ${_s}/LuaSnip
 
     ln -s ${_a}/nvim-notify        ${_s}/nvim-notify
     ln -s ${_a}/Comment.nvim       ${_s}/Comment.nvim
@@ -31,37 +31,37 @@ if test -e $_f ; then
     ln -s ${_a}/vim-pythonx        ${_s}/vim-pythonx
     ln -s ${_a}/cmp-nvim-ultisnips ${_s}/cmp-nvim-ultisnips
 
-    rm ${_ap}/luasnip.lua
-    rm ${_ap}/completion.lua
+    rm -rf ${_ap}/luasnip.lua
+    rm -rf ${_ap}/completion.lua
     ln -s ${_a}/completion_ultisnips.lua ${_ap}/completion.lua
 
-    rm ${_lc}/plugins.lua
+    rm -rf ${_lc}/plugins.lua
     ln -s ${_a}/plugins_ultisnips.lua ${_lc}/plugins.lua
 
-    rm $_f
+    rm -rf $_f
 else
     echo \*LuaSnippets
     echo LuaSnippets > $_f
     ln -s ${_t}.LuaSnippets ${_t}
 
-    rm ${_s}/Comment.nvim
-    rm ${_s}/nvim-notify
-    rm ${_s}/noah.vim
-    rm ${_s}/ultisnips
-    rm ${_s}/vim-snippets
-    rm ${_s}/vim-pythonx
-    rm ${_s}/cmp-nvim-ultisnips
+    rm -rf ${_s}/Comment.nvim
+    rm -rf ${_s}/nvim-notify
+    rm -rf ${_s}/noah.vim
+    rm -rf ${_s}/ultisnips
+    rm -rf ${_s}/vim-snippets
+    rm -rf ${_s}/vim-pythonx
+    rm -rf ${_s}/cmp-nvim-ultisnips
 
     ln -s ${_a}/cmp_luasnip  ${_s}/cmp_luasnip
     ln -s ${_a}/LuaSnip      ${_s}/LuaSnip
     ln -s ${_a}/nvim-notify  ${_s}/nvim-notify
     ln -s ${_a}/Comment.nvim ${_s}/Comment.nvim
 
-    rm ${_ap}/completion.lua
+    rm -rf ${_ap}/completion.lua
     ln -s ${_a}/luasnip.lua ${_ap}/luasnip.lua
     ln -s ${_a}/completion_luasnips.lua ${_ap}/completion.lua
 
-    rm ${_lc}/plugins.lua
+    rm -rf ${_lc}/plugins.lua
     ln -s ${_a}/plugins_luasnip.lua   ${_lc}/plugins.lua
 fi
 
