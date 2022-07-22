cccd0cde74d5c96f1b1806e6cdab0b8a43bfbd9b
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-07-22 16:03:39 -0300

    seleciona packer_compiled.lua

--- bin/snip.neovim.complete.sh
+++ bin/snip.neovim.complete.sh
@@ -4,13 +4,19 @@
 # while true; do sleep 2; clear; test -e snippets && echo EXISTE||echo NÃO EXISTE; done
 
 # if exist file `_f' then remove.
+_t=~/.config/nvim/plugin/packer_compiled.lua
 _f=/tmp/snippets
+
+test -L $_t && rm $_t
+
 if test -e $_f ; then
     echo UltiSnipets
+    ln -s ${_t}.UltiSnipets ${_t}
     rm $_f
 else
     echo LuaSnippets
+    ln -s ${_t}.LuaSnippets ${_t}
     > $_f
 fi
 
-
+exit 0
