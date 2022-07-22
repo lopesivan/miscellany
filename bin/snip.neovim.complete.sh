1fd41810fa8e560f575a1ece6500c80f61d18599
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-07-22 18:06:28 -0300

    Atualiza o arquivo bin/snip.neovim.complete.sh

--- bin/snip.neovim.complete.sh
+++ bin/snip.neovim.complete.sh
@@ -6,8 +6,8 @@
 # if exist file `_f' then remove.
 _t=~/.config/nvim/plugin/packer_compiled.lua
 _f=/tmp/snippets
-_s=$(HOME)/.local/share/nvim/site/pack/packer/start
-_a=$(HOME)/.config/nvim/archive
+_s=${HOME}/.local/share/nvim/site/pack/packer/start
+_a=${HOME}/.config/nvim/archive
 
 test -L $_t && rm $_t
 
@@ -15,32 +15,32 @@ if test -e $_f ; then
     echo UltiSnipets |tee $_f
     ln -s ${_t}.UltiSnipets ${_t}
 
-    ln -s $(_a)/noah.vim           $(_s)/noah.vim
-    ln -s $(_a)/ultisnips          $(_s)/ultisnips
-    ln -s $(_a)/vim-snippets       $(_s)/vim-snippets
-    ln -s $(_a)/vim-pythonx        $(_s)/vim-pythonx
-    ln -s $(_a)/cmp-nvim-ultisnips $(_s)/cmp-nvim-ultisnips
-    ln -s $(_a)/nvim-notify        $(_s)/nvim-notify
-    ln -s $(_a)/Comment.nvim       $(_s)/Comment.nvim
+    ln -s ${_a}/noah.vim           ${_s}/noah.vim
+    ln -s ${_a}/ultisnips          ${_s}/ultisnips
+    ln -s ${_a}/vim-snippets       ${_s}/vim-snippets
+    ln -s ${_a}/vim-pythonx        ${_s}/vim-pythonx
+    ln -s ${_a}/cmp-nvim-ultisnips ${_s}/cmp-nvim-ultisnips
+    ln -s ${_a}/nvim-notify        ${_s}/nvim-notify
+    ln -s ${_a}/Comment.nvim       ${_s}/Comment.nvim
 
-    rm -f $(_s)/cmp_luasnip
-    rm -f $(_s)/LuaSnip
+    rm -f ${_s}/cmp_luasnip
+    rm -f ${_s}/LuaSnip
 
     rm $_f
 else
     echo LuaSnippets |tee $_f
     ln -s ${_t}.LuaSnippets ${_t}
 
-    ln -s $(_a)/cmp_luasnip $(_s)/cmp_luasnip
-    ln -s $(_a)/LuaSnip     $(_s)/LuaSnip
+    ln -s ${_a}/cmp_luasnip ${_s}/cmp_luasnip
+    ln -s ${_a}/LuaSnip     ${_s}/LuaSnip
 
-    rm -f $(_s)/noah.vim
-    rm -f $(_s)/nvim-notify
-    rm -f $(_s)/Comment.nvim
-    rm -f $(_s)/ultisnips
-    rm -f $(_s)/vim-snippets
-    rm -f $(_s)/vim-pythonx
-    rm -f $(_s)/cmp-nvim-ultisnips
+    rm -f ${_s}/noah.vim
+    rm -f ${_s}/nvim-notify
+    rm -f ${_s}/Comment.nvim
+    rm -f ${_s}/ultisnips
+    rm -f ${_s}/vim-snippets
+    rm -f ${_s}/vim-pythonx
+    rm -f ${_s}/cmp-nvim-ultisnips
 fi
 
 exit 0
